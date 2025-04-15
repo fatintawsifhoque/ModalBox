a8af0a2315033e1615244e76ec17acd1740496d7
Fatin Tawsif Hoque <fatintawsifhoque.fatin@gmail.com>
2025-04-15 18:07:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let popUp = document.getElementById("popup");
+
+function openPopUp()
+    {
+    popUp.classList.add("openPopUp");
+}
+function closePopUp()
+    {
+    popUp.classList.remove("openPopUp");
+}
